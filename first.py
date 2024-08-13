5daac77b2247ebc5946504eebd4826c1cbfbbb97
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-13 23:29:52 +0500

    changes

--- first.py
+++ first.py
@@ -53,3 +53,19 @@ print(type(d))
 import random
 
 print(random.randrange(1, 10))
+
+#multi line strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
