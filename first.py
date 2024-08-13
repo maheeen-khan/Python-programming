f9d9d1b9c064418d7aeab875f1cca2ffc6b1e0d4
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-13 21:11:50 +0500

    changes

--- first.py
+++ first.py
@@ -7,3 +7,15 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+
+#If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+
+a = "awesome"
+
+def myfunc():
+  a = "fantastic"
+  print("Python is " + a)
+
+myfunc()
+
+print("Python is " + a)
