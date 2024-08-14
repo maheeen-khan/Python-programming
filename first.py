ce9659d81c45b7e7905bee88c6abe577ca566c0e
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-14 16:02:51 +0500

    Changes

--- first.py
+++ first.py
@@ -69,3 +69,12 @@ print(type(thistuple))
 #NOT a tuple
 thistuple = ("apple")
 print(type(thistuple))
+
+
+#................Keyword Argument...........
+
+def display(**kwargs):
+    for i in kwargs:
+        print(i)
+
+display(emp="Kelly", salary=9000)
